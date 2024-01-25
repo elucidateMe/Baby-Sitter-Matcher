428b3d016ae38285d326a39e6dfe17e2fb958118
elucidateMe <155992957+elucidateMe@users.noreply.github.com>
2024-01-25 13:26:32 -0500

    Update sitterClass.py
    changed variable names as to be more readable

--- sitterClass.py
+++ sitterClass.py
@@ -1,13 +1,13 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import distance
 
-class rSitter:
-	def __init__(self, location, pDistance, pAge, pTime, name):
+class sitterClass:
+	def __init__(self, location, preferredDistance, preferredAge, preferredTime, name):
                 geolocator = Nominatim(user_agent='distance_calculator')
                 self.location = geolocator.geocode(location, timeout = None)
-                self.pDistance = pDistance
-                self.pAge = pAge
-                self.pTime = pTime
+                self.preferredDistance = preferredDistance
+                self.preferredAge = preferredAge
+                self.preferredTime = preferredTime
                 self.name = name
 
 """
