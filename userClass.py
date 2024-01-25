24a8ce6cb679c6644783f30893e7d7c30ea517e3
elucidateMe <155992957+elucidateMe@users.noreply.github.com>
2024-01-25 14:29:23 -0500

    Update userClass.py

--- userClass.py
+++ userClass.py
@@ -2,12 +2,12 @@ from geopy.geocoders import Nominatim
 from geopy.distance import distance
 
 class userClass:
-	def __init__(self, location, age, time, name):
-                geolocator = Nominatim(user_agent='distance_calculator')
-                self.location = geolocator.geocode(location, timeout = None)
-                self.age = age
-                self.time = time
-                self.name = name
+    def __init__(self, location, age, time, name):
+        geolocator = Nominatim(user_agent='distance_calculator')
+        self.location = geolocator.geocode(location, timeout = None)
+        self.age = age
+        self.time = time
+        self.name = name
 
 """
 location format 'street address, city, state zip, country'
