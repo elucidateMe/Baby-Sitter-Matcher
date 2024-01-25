be80a5736ec0b41076a621ac3db97ff7595a8a08
elucidateMe <155992957+elucidateMe@users.noreply.github.com>
2024-01-25 13:24:33 -0500

    Create userClass.py

---
+++ userClass.py
@@ -0,0 +1,17 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import distance
+
+class rUser:
+	def __init__(self, location, age, time, name):
+                geolocator = Nominatim(user_agent='distance_calculator')
+                self.location = geolocator.geocode(location, timeout = None)
+                self.age = age
+                self.time = time
+                self.name = name
+
+"""
+location format 'street address, city, state zip, country'
+will be stored as coordinates, accessed with .latitude and .longitude
+Age can be an integer value
+time will be two times, initially in 24hour format for conveniences sake
+"""
